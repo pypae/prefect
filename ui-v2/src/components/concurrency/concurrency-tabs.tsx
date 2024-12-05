6d4c42a8a5bc155210a3246e48a502de684c8de1
Devin Villarosa <devinvillarosa92.business@gmail.com>
2024-12-04 17:38:28 -0800

    [UI v2] feat: Loads data and url state to concurrency limit route for UX to consume

--- ui-v2/src/components/concurrency/concurrency-tabs.tsx
+++ ui-v2/src/components/concurrency/concurrency-tabs.tsx
@@ -1,31 +1,55 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import { TAB_OPTIONS, TabOptions } from "./concurrency-constants";
+import { TabOptions } from "@/routes/concurrency-limits";
+import { getRouteApi } from "@tanstack/react-router";
+
+const routeApi = getRouteApi("/concurrency-limits");
+
+/** Maps url tab option to visual name */
+const TAB_OPTIONS: Record<TabOptions, TabOptions> = {
+	Global: "Global",
+	"Task Run": "Task Run",
+} as const;
 
 type Props = {
 	globalView: React.ReactNode;
-	onValueChange: (value: TabOptions) => void;
 	taskRunView: React.ReactNode;
-	value: TabOptions;
 };
 
 // TODO: Move Tabs for navigation to a generic styled component
 
 export const ConcurrencyTabs = ({
 	globalView,
-	onValueChange,
 	taskRunView,
-	value,
 }: Props): JSX.Element => {
+	const { tab } = routeApi.useSearch();
+	const navigate = routeApi.useNavigate();
+
 	return (
-		<Tabs
-			defaultValue="Global"
-			className="w-[400px]"
-			value={value}
-			onValueChange={(value) => onValueChange(value as TabOptions)}
-		>
+		<Tabs defaultValue="Global" className="w-[400px]" value={tab}>
 			<TabsList className="grid w-full grid-cols-2">
-				<TabsTrigger value="Global">{TAB_OPTIONS.Global}</TabsTrigger>
-				<TabsTrigger value="Task Run">{TAB_OPTIONS["Task Run"]}</TabsTrigger>
+				<TabsTrigger
+					value={TAB_OPTIONS.Global}
+					onClick={() => {
+						void navigate({
+							to: "/concurrency-limits",
+							search: (prev) => ({ ...prev, tab: TAB_OPTIONS.Global }),
+						});
+					}}
+				>
+					{TAB_OPTIONS.Global}
+				</TabsTrigger>
+
+				<TabsTrigger
+					value={TAB_OPTIONS["Task Run"]}
+					onClick={() => {
+						void navigate({
+							to: "/concurrency-limits",
+							search: (prev) => ({ ...prev, tab: TAB_OPTIONS["Task Run"] }),
+						});
+					}}
+				>
+					{TAB_OPTIONS["Task Run"]}
+				</TabsTrigger>
 			</TabsList>
 			<TabsContent value={TAB_OPTIONS.Global}>{globalView}</TabsContent>
 			<TabsContent value={TAB_OPTIONS["Task Run"]}>{taskRunView}</TabsContent>
